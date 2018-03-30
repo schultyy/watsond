7fc3fd1d01498d41417600a7b065eda45afef4d0
Jan Schulte <hello@unexpected-co.de>
2018-03-30 12:45:23 +0200

    feat: Retrieve file

--- src/http_server.rs
+++ src/http_server.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use std::sync::Mutex;
 use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct File {
     content: String,
     name: String
@@ -28,9 +28,29 @@ pub fn add_file(file: Json<File>, file_list: State<FileList>) -> Json<Value> {
   Json(json!({ "status": "ok", "id": new_id.to_string() }))
 }
 
+#[get("/file/<uuid>", format = "application/json")]
+pub fn get(uuid: String, file_list: State<FileList>) -> Option<Json<File>> {
+  if let Ok(file_uuid) = Uuid::parse_str(&uuid) {
+    let hashmap = file_list.lock().unwrap();
+    hashmap.get(&file_uuid).map(|file| { Json(file.clone()) })
+  }
+  else {
+    None
+  }
+}
+
+#[error(404)]
+fn not_found() -> Json<Value> {
+  Json(json!({
+    "status": "error",
+    "reason": "Resource was not found."
+  }))
+}
+
 pub fn rocket() -> Rocket {
   rocket::ignite()
-      .mount("/", routes![status, add_file])
+      .mount("/", routes![status, add_file, get])
+      .catch(errors![not_found])
       .manage(Mutex::new(HashMap::<ID, File>::new()))
 }
 
@@ -62,5 +82,33 @@ mod test {
     assert_eq!(response.status(), Status::Ok);
     let v: Value = serde_json::from_str(&response.body_string().unwrap()).unwrap();
     assert!(Uuid::parse_str(v["id"].as_str().unwrap()).is_ok());
+  }
+
+  #[test]
+  fn retrieve_existing_file() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.post("/file")
+        .header(ContentType::JSON)
+        .body(r#"{ "name": "support_bundle/1354/container/foo.log", "content": "Test1234" }"#)
+        .dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    let v: Value = serde_json::from_str(&response.body_string().unwrap()).unwrap();
+    let id = Uuid::parse_str(v["id"].as_str().unwrap()).unwrap();
+
+    let mut response = client.get(format!("/file/{}", id)).header(ContentType::JSON).dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let file_response: Value = serde_json::from_str(&response.body_string().unwrap()).unwrap();
+
+    assert_eq!(file_response["name"].as_str(), Some("support_bundle/1354/container/foo.log"));
+    assert_eq!(file_response["content"].as_str(), Some("Test1234"));
+  }
+
+  #[test]
+  fn retrieve_nonexisting_file() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.get(format!("/file/{}", "abc")).header(ContentType::JSON).dispatch();
+    assert_eq!(response.status(), Status::NotFound);
+
   }
 }
