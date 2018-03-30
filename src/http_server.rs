9c16de2c2ca91fad282adb361058d733208ac3af
Jan Schulte <hello@unexpected-co.de>
2018-03-30 12:15:42 +0200

    feat: return a uuid for each added file

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,7 +1,9 @@
 use rocket;
 use rocket::{Rocket, State};
 use rocket_contrib::{Json, Value};
+use std::collections::HashMap;
 use std::sync::Mutex;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct File {
@@ -9,7 +11,8 @@ pub struct File {
     name: String
 }
 
-type FileList = Mutex<Vec<File>>;
+type ID = Uuid;
+type FileList = Mutex<HashMap<ID, File>>;
 
 #[get("/status")]
 pub fn status() -> &'static str {
@@ -18,23 +21,27 @@ pub fn status() -> &'static str {
 
 #[post("/file", format = "application/json", data = "<file>")]
 pub fn add_file(file: Json<File>, file_list: State<FileList>) -> Json<Value> {
-  let mut list = file_list.lock().expect("map lock.");
-  list.push(file.0);
+  let mut file_list = file_list.lock().expect("map lock.");
+  let new_id = Uuid::new_v4();
+  file_list.insert(new_id, file.0);
 
-  Json(json!({ "status": "ok" }))
+  Json(json!({ "status": "ok", "id": new_id.to_string() }))
 }
 
 pub fn rocket() -> Rocket {
   rocket::ignite()
       .mount("/", routes![status, add_file])
-      .manage(Mutex::new(Vec::<File>::new()))
+      .manage(Mutex::new(HashMap::<ID, File>::new()))
 }
 
 #[cfg(test)]
 mod test {
+  use super::Uuid;
   use super::rocket;
   use rocket::local::Client;
   use rocket::http::{Status, ContentType};
+  extern crate serde_json;
+  use self::serde_json::Value;
 
   #[test]
   fn status() {
@@ -47,10 +54,13 @@ mod test {
   #[test]
   fn add_file() {
     let client = Client::new(rocket()).expect("valid rocket instance");
-    let response = client.post("/file")
+    let mut response = client.post("/file")
         .header(ContentType::JSON)
         .body(r#"{ "name": "support_bundle/1354/container/foo.log", "content": "Test1234" }"#)
         .dispatch();
+
     assert_eq!(response.status(), Status::Ok);
+    let v: Value = serde_json::from_str(&response.body_string().unwrap()).unwrap();
+    assert!(Uuid::parse_str(v["id"].as_str().unwrap()).is_ok());
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
+extern crate uuid;
 
 mod http_server;
 
