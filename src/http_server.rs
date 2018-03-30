9a5a754429532e5ec464962d7bf966ad7da58326
Jan Schulte <hello@unexpected-co.de>
2018-03-30 11:35:59 +0200

    feat: Add /file endpoint

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,20 +1,40 @@
 use rocket;
-use rocket::Rocket;
+use rocket::{Rocket, State};
+use rocket_contrib::{Json, Value};
+use std::sync::Mutex;
+
+#[derive(Deserialize)]
+pub struct File {
+    content: String,
+    name: String
+}
+
+type FileList = Mutex<Vec<File>>;
 
 #[get("/status")]
 pub fn status() -> &'static str {
   "alive"
 }
 
+#[post("/file", format = "application/json", data = "<file>")]
+pub fn add_file(file: Json<File>, file_list: State<FileList>) -> Json<Value> {
+  let mut list = file_list.lock().expect("map lock.");
+  list.push(file.0);
+
+  Json(json!({ "status": "ok" }))
+}
+
 pub fn rocket() -> Rocket {
-  rocket::ignite().mount("/", routes![status])
+  rocket::ignite()
+      .mount("/", routes![status, add_file])
+      .manage(Mutex::new(Vec::<File>::new()))
 }
 
 #[cfg(test)]
 mod test {
   use super::rocket;
   use rocket::local::Client;
-  use rocket::http::Status;
+  use rocket::http::{Status, ContentType};
 
   #[test]
   fn status() {
@@ -22,5 +42,15 @@ mod test {
     let mut response = client.get("/status").dispatch();
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.body_string(), Some("alive".into()));
+  }
+
+  #[test]
+  fn add_file() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.post("/file")
+        .header(ContentType::JSON)
+        .body(r#"{ "name": "support_bundle/1354/container/foo.log", "content": "Test1234" }"#)
+        .dispatch();
+    assert_eq!(response.status(), Status::Ok);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
 mod http_server;
 
 fn main() {
